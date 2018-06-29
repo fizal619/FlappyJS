af9ce11c4f09796b25fc69256c0556bbf0a3b43c
John Murphy <murf2073@gmail.com>
2018-06-29 16:01:06 -0400

    mobile friendly version part 2

--- main.js
+++ main.js
@@ -11,6 +11,14 @@ let mainState = {
 	},
 
 	create : function() {
+		// if it's a mobile device, scale to show all, set sizes, and center
+		if(game.device.desktop == false){
+			game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+			game.scale.setMinMax(game.width/2, game.height/2, game.width, game.height);
+			game.scale.pageAlignHorizontally = true;
+			game.scale.pageAlignVertically = true;
+		}
+
 		// set background color
 		game.stage.backgroundColor = 'rgb(113, 197, 207)';
 
