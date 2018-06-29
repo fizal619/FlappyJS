bfce2becc7d4194870dc10c1d3325be8e94afb71
John Murphy <murf2073@gmail.com>
2018-06-29 16:43:03 -0400

    commented the server setup

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ var express = require('express');
 var app = express();
 var http = require('http');
 
+// set up the server to run on port 8080
 var server = http.Server(app);
 server.listen(process.env.PORT || 8080, function() {
 	console.log('Server started at http://localhost:8080');
 });
 
+// make the assets folder available
 app.use('/assets', express.static(__dirname + '/assets'));
 
+// serve index.html in response to a get request at the root
 app.get('/', function(request, response) {
     response.sendfile(__dirname + '/index.html');
 });
